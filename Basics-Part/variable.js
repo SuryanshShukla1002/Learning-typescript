b4d1eb26dcfd90c3d6a32af001a211701eb9cf7a
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-07-19 10:53:42 +0530

    number boolean and type inference

--- Basics-Part/variable.js
+++ Basics-Part/variable.js
@@ -1,2 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greeting = "Hey there its suryansh shukla";
+// greeting = 4  will give an error check as greeting is a string type
 console.log(greeting);
+// number
+var userId = 3456;
+// boolean
+var isloggedIn = false;
+isloggedIn.valueOf();
--- Basics-Part/variable.ts
+++ Basics-Part/variable.ts
@@ -2,4 +2,12 @@ let greeting: string = "Hey there its suryansh shukla"
 // greeting = 4  will give an error check as greeting is a string type
 console.log(greeting);
 
+// number
+let userId: number = 3456
+
+// boolean
+let isloggedIn: boolean = false
+isloggedIn.valueOf()
+
+
 export {}
