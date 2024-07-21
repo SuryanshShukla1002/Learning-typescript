4cf15c444d0a87421d1c52215b648cd901e6b23e
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-07-21 12:56:45 +0530

    readonly and optional

---
+++ Type-Aliases/typeAliases.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var createUser = function (user) { };
+createUser({ name: "Suryansh", age: 18, isPaid: true });
+var myUser = {
+    _id: "abc",
+    name: "Surya",
+    age: 19,
+    isPaid: false
+};
+// myUser._id = "cde" will not allow as it is readonly property
---
+++ Type-Aliases/typeAliases.ts
@@ -0,0 +1,29 @@
+type User = {
+  name: string;
+  age: number;
+  isPaid: boolean;
+};
+
+const createUser = (user: User) => {}
+
+createUser({name:"Suryansh" , age: 18, isPaid: true})
+
+export {};
+
+type users = {
+  readonly _id: string,
+  name: string;
+  age: number;
+  isPaid: boolean;
+  credcard?: number
+}
+
+let myUser: users = {
+  _id: "abc",
+  name: "Surya",
+  age: 19,
+  isPaid: false
+}
+// myUser._id = "cde" will not allow as it is readonly property
+
+
