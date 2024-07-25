7ceb663dbdbe72f4fdbfb590d68f3e9b60223354
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-07-25 19:40:32 +0530

    type vs interface

--- myInterface.js
+++ myInterface.js
@@ -1,3 +1,12 @@
-var S = { dbID: 3, email: "h@h.com", userID: 4, startTrail: function () {
+var S = {
+    dbID: 3,
+    email: "h@h.com",
+    userID: 4,
+    githubToken: "github",
+    startTrail: function () {
         return "Hello there";
-    } };
+    },
+    getCoupen: function () {
+        return 9;
+    },
+};
--- myInterface.ts
+++ myInterface.ts
@@ -1,12 +1,25 @@
 interface User {
-    readonly dbID: number,
-    email: string,
-    userID: number,
-    googleID?: number,
-    startTrail(): string
+  readonly dbID: number;
+  email: string;
+  userID: number;
+  googleID?: number;
+  startTrail(): string;
+  getCoupen(coupen: string): number;
 }
 
-const S: User = {dbID: 3, email: "h@h.com", userID: 4 , startTrail: () => {
-    return "Hello there"
-}}
+interface User {
+    githubToken: string
+}
 
+const S: User = {
+  dbID: 3,
+  email: "h@h.com",
+  userID: 4,
+  githubToken: "github",
+  startTrail: () => {
+    return "Hello there";
+  },
+  getCoupen: () => {
+    return 9;
+  },
+};
