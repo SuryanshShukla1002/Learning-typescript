ffb571239658f6aeb2eefc808d3fc97cb4a7b843
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-03 23:56:23 +0530

    Protected in typeScript

---
+++ Medium/src/index.js
@@ -0,0 +1,52 @@
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = "Jaipur"
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+//     }
+// }
+var User = /** @class */ (function () {
+    function User(email, name) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        this.city = "Jaipur";
+    }
+    User.prototype.deleteToken = function () {
+        console.log("Token deleted");
+    };
+    return User;
+}());
+//  Protected in typescript
+var SubUser = /** @class */ (function (_super) {
+    __extends(SubUser, _super);
+    function SubUser() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.isFamily = true;
+        return _this;
+    }
+    SubUser.prototype.changeCourseCount = function () {
+        this._courseCount = 4;
+    };
+    return SubUser;
+}(User));
+var hitesh = new User("h@h.com", "hitesh");
+// hitesh.name
+// hitesh.deleteToken()
--- Medium/src/index.ts
+++ Medium/src/index.ts
@@ -9,45 +9,28 @@
 //     }
 // }
 class User {
+  protected _courseCount = 1;
 
-    protected _courseCount = 1
-    
-    readonly city: string = "Jaipur"
-    constructor(
-        public email: string, 
-        public name: string,
-        // private userId: string
-        ){
-    }
-    private deleteToken(){
-        console.log("Token deleted");
-        
-    }
-
-    get getAppleEmail(): string{
-        return `apple${this.email}`
-    }
-
-    get courseCount(): number {
-        return this._courseCount
-    }
-
-    set courseCount(courseNum) {
-        if (courseNum <= 1) {
-            throw new Error("Course count should be more than 1")
-        }
-        this._courseCount = courseNum
-    }
+  readonly city: string = "Jaipur";
+  constructor(
+    public email: string,
+    public name: string
+  ) // private userId: string
+  {}
+  private deleteToken() {
+    console.log("Token deleted");
+  }
 }
 
+//  Protected in typescript
 class SubUser extends User {
-    isFamily: boolean = true
-    changeCourseCount(){
-        this._courseCount = 4
-    }
+  isFamily: boolean = true;
+  changeCourseCount() {
+    this._courseCount = 4;
+  }
 }
 
-const hitesh = new User("h@h.com","hitesh")
-// hitesh.name 
+const hitesh = new User("h@h.com", "hitesh");
+// hitesh.name
 
 // hitesh.deleteToken()
