238a08c3795c29d32f6c887aafbe2c422c3ce22f
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-06 23:59:41 +0530

    functions in generatics type return

---
+++ Medium/src/myGenerics.ts
@@ -0,0 +1,40 @@
+const score: Array<number> = []
+const names:Array<string> = []
+
+function identityOne(val: boolean | number): boolean | number{
+    return val
+}
+
+function identityTwo(val: any):any{
+    return val
+}
+
+function identityThree<Type>(val: Type): Type {
+    return val
+}
+
+// identityThree(true)
+
+function identityFour<T>(val: T): T {
+    return val
+}
+
+interface Bootle{
+    brand: string,
+    type: number
+}
+
+// identityFour<Bootle>({})
+
+function getSearchProducts<T>(products: T[]): T {
+    // do some database operations
+    const myIndex = 3
+    return products[myIndex]
+}
+
+
+const getMoreSearchProducts = <T,>(products: T[]): T => {
+    //do some database operations
+    const myIndex = 4
+    return products[myIndex]
+}
